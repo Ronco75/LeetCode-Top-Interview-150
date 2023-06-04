d088030b584d450125ade28a6cac56c2e9804e7a
[Ron Cohen] <[ronc7500@gmail.com]>
2023-06-04 23:19:45 +0300

    2

---
+++ src/RemoveElement.java
@@ -0,0 +1,15 @@
+public class RemoveElement {
+
+    public int removeElement(int[] nums, int val) {
+        int temp = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[temp] = nums[i];
+                temp++;
+            }
+        }
+            return temp;
+        }
+    }
+

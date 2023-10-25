21de893879d2eaf9280414c86f5982f9353ceabd
[Ron Cohen] <[ronc7500@gmail.com]>
2023-10-25 14:19:06 +0300

    6

---
+++ src/LengthOfLastWord.java
@@ -0,0 +1,17 @@
+public class LengthOfLastWord {
+
+    public int LengthOfLastWord(String s) {
+
+        String str = s.trim();
+        int count = 0;
+        for (int i = str.length() - 1; i >= 0; i--) {
+            if (str.charAt(i) != ' ') {
+                count++;
+            } else {
+                break;
+            }
+        }
+        return count;
+    }
+
+}

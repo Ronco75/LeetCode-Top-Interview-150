1a44710fa7ea53a8d576efeb0174450a20572806
[Ron Cohen] <[ronc7500@gmail.com]>
2023-11-26 14:31:29 +0200

    11

---
+++ src/FindTheIndexOfTheFirstOccurrenceInAString.java
@@ -0,0 +1,7 @@
+public class FindTheIndexOfTheFirstOccurrenceInAString {
+
+    public int FindTheIndexOfTheFirstOccurrenceInAString(String haystack, String needle) {
+        int index = haystack.indexOf(needle);
+        return index;
+    }
+}

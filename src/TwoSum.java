cc6aa12b69a87ac5bdc1c8d17e0ed2fe58658a18
Ron Cohen <ronc7500@gmail.com>
2024-04-15 10:48:59 +0300

    improve twoSum

--- src/TwoSum.java
+++ src/TwoSum.java
@@ -1,16 +1,26 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TwoSum {
     public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> complementToTarget = new HashMap<>();
 
-        for(int i = 0; i < nums.length;i ++){
-            for(int j = i + 1; j < nums.length;j++){
-                if(nums[i] + nums[j] == target) {
-                    return new int[]{i, j};
-                }
+        for (int i = 0; i < nums.length; i++) {
+            if (complementToTarget.containsKey(nums[i])) {
+                return new int[]{complementToTarget.get(nums[i]), i};
             }
+
+            complementToTarget.put(target - nums[i], i);
         }
         return new int[]{-1,-1};
     }
 
+    public static void main(String[] args) {
+        int[] arr = {2,7,11,15};
+        int target = 9;
+        System.out.println(Arrays.toString(twoSum(arr, target)));
+    }
 }
+
+

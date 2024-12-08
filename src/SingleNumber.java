025963bd2190e67a6d7e6ffefcdab3e10530fbe6
Ron Cohen <ronc7500@gmail.com>
2024-12-08 18:54:46 +0200

    22

---
+++ src/SingleNumber.java
@@ -0,0 +1,9 @@
+public class SingleNumber {
+    public int singleNumber(int[] nums) {
+        int res = 0;
+        for (int n : nums) {
+            res ^= n;
+        }
+        return res;
+    }
+}

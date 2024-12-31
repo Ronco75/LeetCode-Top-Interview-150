fb73b83b63349daafff742602e205f4516e2feda
Ron Cohen <ronc7500@gmail.com>
2024-12-31 13:56:27 +0200

    Delete main

--- src/TwoSum.java
+++ src/TwoSum.java
@@ -15,12 +15,6 @@ public class TwoSum {
         }
         return new int[]{-1,-1};
     }
-
-    public static void main(String[] args) {
-        int[] arr = {2,7,11,15};
-        int target = 9;
-        System.out.println(Arrays.toString(twoSum(arr, target)));
-    }
 }
 
 
